71965008451038d96c27a690e6edd7c37e2f193f
KikeViqueira <enrique.viqueira@rai.usc.es>
2024-10-04 20:02:55 +0200

    prueba

--- src/main/java/com/example/proyectoparte1/ProyectoParte1Application.java
+++ src/main/java/com/example/proyectoparte1/ProyectoParte1Application.java
@@ -8,6 +8,7 @@ public class ProyectoParte1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(ProyectoParte1Application.class, args);
+        System.out.println("holla");
     }
 
 
